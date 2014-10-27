17ae1580f361ba5843c93597d3f130d656843a7c
David Andrews <david@ryatta.com>
2014-10-27 09:49:24 -0400

    First pass refactor.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,57 +1,42 @@
 class GildedRose
   attr_reader :items
+  MAX_QUALITY = 50
+  MIN_QUALITY = 0
   
   def initialize item_attributes
     @items = item_attributes.map { |args| Item.new(*args) }
   end
 
-  def update_quality
-
-    for i in 0..(@items.size-1)
-      if (@items[i].name != "Aged Brie" && @items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-        if (@items[i].quality > 0)
-          if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-            @items[i].quality = @items[i].quality - 1
-          end
-        end
-      else
-        if (@items[i].quality < 50)
-          @items[i].quality = @items[i].quality + 1
-          if (@items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-            if (@items[i].sell_in < 11)
-              if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
-              end
-            end
-            if (@items[i].sell_in < 6)
-              if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
-              end
-            end
-          end
-        end
-      end
-      if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-        @items[i].sell_in = @items[i].sell_in - 1;
-      end
-      if (@items[i].sell_in < 0)
-        if (@items[i].name != "Aged Brie")
-          if (@items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-            if (@items[i].quality > 0)
-              if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-                @items[i].quality = @items[i].quality - 1
-              end
-            end
-          else
-            @items[i].quality = @items[i].quality - @items[i].quality
-          end
-        else
-          if (@items[i].quality < 50)
-            @items[i].quality = @items[i].quality + 1
-          end
-        end
-      end
+  def update_attributes_for_new_day
+    @items.each do |item|
+      update_quality item
+      update_sell_in item
     end
   end
 
+  def update_quality item
+    is_stale = (item.sell_in <= 0)
+    case item.name
+    when "Aged Brie" # Aged
+      quality_increment = is_stale ? 2 : 1
+      item.quality = [item.quality + quality_increment, MAX_QUALITY].min
+    when "Backstage passes to a TAFKAL80ETC concert" # Backstage passes
+      conditions = [item.sell_in < 11, item.sell_in < 6]
+      quality_increment = 1 + conditions.count(true)
+      item.quality = [item.quality + quality_increment, MAX_QUALITY].min
+      item.quality = MIN_QUALITY if is_stale
+    when "Sulfuras, Hand of Ragnaros" # Legendary
+      # do nothing
+    when "Conjured Mana Cake" # Conjured
+      quality_decrement = is_stale ? 4 : 2
+      item.quality = [item.quality - quality_decrement, MIN_QUALITY].max
+    else # Regular items
+      quality_decrement = is_stale ? 2 : 1
+      item.quality = [item.quality - quality_decrement, MIN_QUALITY].max
+    end
+  end
+
+  def update_sell_in item
+    item.sell_in -= 1 unless item.name == "Sulfuras, Hand of Ragnaros"
+  end
 end
--- spec/gilded_rose_characterization_spec.rb
+++ spec/gilded_rose_characterization_spec.rb
@@ -11,13 +11,13 @@ describe GildedRose do
 
     verify { characterize(subject, 20) }
   end
-
+  
 end
 
 private
 
 def item_attributes
-  item_attributes = [
+  [
     ['Mail Armour', 10, 20],
     ['Mail Armour', 10, 1],
     ["+5 Dexterity Vest", 10, 20],
@@ -26,7 +26,18 @@ def item_attributes
     ['Aged Brie', 1, 49],
     ['Backstage passes to a TAFKAL80ETC concert', 15, 17],
     ['Backstage passes to a TAFKAL80ETC concert', 5, 49],
-    ['Sulfuras, Hand of Ragnaros', -1, 80]
+    ['Sulfuras, Hand of Ragnaros', -1, 80]#,
+#    ['Conjured Mana Cake', 10, 20],
+#    ['Aged Stilton', 4, 9],
+#    ['Aged Stilton', 1, 49],
+#    ['Aged Elvish Wine', 4, 9],
+#    ['Aged Elvish Wine', 1, 49],
+#    ['Backstage passes to the Rat Queens', 15, 17],
+#    ['Backstage passes to the Rat Queens', 5, 49],
+#    ['Backstage passes to the Hard Rock Troll Orchestra', 15, 17],
+#    ['Backstage passes to the Hard Rock Troll Orchestra', 5, 49],
+#    ['Conjured loincloth', 10, 20],
+#    ['Conjured fake beard', 10, 20]
   ]
 end
 
@@ -34,7 +45,7 @@ def characterize(subject, days)
   characterization = []
 
   (1..days).each_with_index do |day|
-    subject.update_quality
+    subject.update_attributes_for_new_day
 
     characterization << "Day #{day} of #{days}"
     subject.items.each { |item| characterization << "  #{item.to_s}" }
